6a210b604905cc47dd8bb9f0ee760acda7208dfe
Marilee Zafaripour <71002451+MarileeZ@users.noreply.github.com>
2022-01-27 18:48:16 -0800

    Add files via upload

---
+++ marileez_p1.py
@@ -0,0 +1,33 @@
+'''
+Program: Project 1 - Stacks and Queues
+Author: Marilee Zafaripour
+Date: 01/22/2021
+Description: 
+
+Notes:
+'''
+from typing import Generic
+
+
+class Node:
+
+    def __init__(Node, Generic):
+        self.node = Node
+        self.generic = Generic
+    pass
+
+class Queue:
+
+    def __init__(Queue, Int):
+        self.queue = Queue
+        self.int = Int
+    pass
+
+class Stack:
+    
+    def __init__(Stack, Int):
+    pass
+
+
+
+
